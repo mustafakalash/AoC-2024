e0fd4ea093b53e32a1ae3596fa99dc4a95ef58e3
Mustafa Kalash <me@mkalash.com>
2024-12-02 08:15:50 -0500

    Day 1

---
+++ 1/main.py
@@ -0,0 +1,19 @@
+with open("1/input") as f:
+    list1 = []
+    list2 = []
+    for line in f:
+        nums = line.split("   ")
+        list1.append(int(nums[0]))
+        list2.append(int(nums[1]))
+
+    list1.sort()
+    list2.sort()
+    
+    sum = 0
+    sim = 0
+    for i in range(len(list1)):
+        sum += abs(list1[i] - list2[i])
+        sim += list1[i] * list2.count(list1[i])
+
+    print(f"Part 1: {sum}")
+    print(f"Part 2: {sim}")
